d8459219a9c4cae00dc04fc0b7119a4f31f3788e
emre757 <188556882+emre757@users.noreply.github.com>
2026-09-04 00:27:54 +0200

    chore: fix statemapper and remove remaining unused components

--- app/Mappers/GameStateMapper.php
+++ app/Mappers/GameStateMapper.php
@@ -19,7 +19,9 @@ final readonly class GameStateMapper
             status: $game->status,
             board: Board::fromArray($game->board),
             winner: $game->winner,
-            forcedCaptureFrom: Position::fromArray($game->forced_capture_from) ?? null,
+            forcedCaptureFrom: $game->forced_capture_from !== null
+                ? Position::fromArray($game->forced_capture_from)
+                : null,
         );
     }
 }
